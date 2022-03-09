e90390e6f9b87534cb0fb7e4662818a60ccb49ff
NHawk-Ke <kevingenginburnaby@gmail.com>
2022-03-08 20:03:40 -0500

    fix import

--- anomaly_detection_general.py
+++ anomaly_detection_general.py
@@ -8,40 +8,10 @@ import numpy as np
 import pandas as pd
 import torch
 import torch.multiprocessing as mp
-import torch.nn as nn
+from model import AutoEncoder
 
 from util import compute_tensor, compute_tensor_byte, filter_ip, filter_dest_port, filter_uniq
 from util import parallelize_dataframe, convert_timestamp, get_model_type
-
-
-class AutoEncoder(nn.Module):
-    def __init__(self):
-        super(AutoEncoder, self).__init__()
-        self.encoder = nn.Sequential(
-            nn.Conv3d(1, 32, 3, stride=2, padding=1),
-            nn.ReLU(),  # or SELU()
-            nn.Conv3d(32, 64, 3, stride=2, padding=1),
-            nn.ReLU(),
-            nn.Conv3d(64, 128, 3, stride=2, padding=1),
-            nn.ReLU(),
-            nn.Conv3d(128, 256, 3, stride=2, padding=1),
-            nn.ReLU()
-        )
-        self.decoder = nn.Sequential(
-            nn.ConvTranspose3d(256, 128, 3, stride=2, padding=1, output_padding=1),
-            nn.ReLU(),
-            nn.ConvTranspose3d(128, 64, 3, stride=2, padding=1, output_padding=1),
-            nn.ReLU(),
-            nn.ConvTranspose3d(64, 32, 3, stride=2, padding=1, output_padding=1),
-            nn.ReLU(),
-            nn.ConvTranspose3d(32, 1, 3, stride=2, padding=1, output_padding=1),
-            nn.ReLU()
-        )
-
-    def forward(self, x):
-        x = self.encoder(x)
-        x = self.decoder(x)
-        return x
 
 
 def get_index(flatten_idx):
